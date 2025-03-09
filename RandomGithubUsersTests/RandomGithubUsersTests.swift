c9225e33ae2fe185ec7317e0e413b7901ca66592
fajar <fajariyandi36@gmail.com>
2025-03-09 16:26:28 +0700

    Initial Commit

---
+++ RandomGithubUsersTests/RandomGithubUsersTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomGithubUsersTests.swift
+//  RandomGithubUsersTests
+//
+//  Created by fajar on 09/03/25.
+//
+
+import Testing
+@testable import RandomGithubUsers
+
+struct RandomGithubUsersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
